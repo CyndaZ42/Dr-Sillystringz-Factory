76ce70bd1a5317445013fcc753761e059eddb882
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-08-05 13:16:05 -0700

    Add ability to view details

--- Factory/Controllers/EngineerController.cs
+++ Factory/Controllers/EngineerController.cs
@@ -34,5 +34,14 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+        .Include(engineer => engineer.JoinEntities)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
   }
 }
--- Factory/Controllers/MachineController.cs
+++ Factory/Controllers/MachineController.cs
@@ -34,5 +34,14 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      var thisMachine = _db.Machines
+        .Include(machine => machine.JoinEntities)
+        .ThenInclude(join => join.Engineer)
+        .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
   }
 }
